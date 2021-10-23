a2b82ae37b15b4be40e063af602d1ebb4031a0eb
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-24 00:15:55 +0100

    first commit

---
+++ src/main/java/mo/spring/ms/hpcbillingservice/HpcBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.ms.hpcbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcBillingServiceApplication.class, args);
+    }
+
+}
