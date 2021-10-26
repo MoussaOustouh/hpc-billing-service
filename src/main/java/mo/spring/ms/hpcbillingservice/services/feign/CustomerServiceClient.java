dff926ab99075c566f90778ed814b91b31775588
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-26 02:30:47 +0100

    change packages

--- src/main/java/mo/spring/ms/hpcbillingservice/services/feign/CustomerServiceClient.java
+++ src/main/java/mo/spring/ms/hpcbillingservice/services/feign/CustomerServiceClient.java
@@ -1,11 +1,11 @@
-package mo.spring.ms.hpcbillingservice.services;
+package mo.spring.ms.hpcbillingservice.services.feign;
 
 import mo.spring.ms.hpcbillingservice.models.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-services")
+@FeignClient(name = "CUSTOMER-SERVICES")
 public interface CustomerServiceClient {
     @GetMapping("?customers/{id}?projection=fullCustomer")
     Customer findCustomerById(@PathVariable("id") Long id);
--- src/main/java/mo/spring/ms/hpcbillingservice/services/feign/InventoryServiceClient.java
+++ src/main/java/mo/spring/ms/hpcbillingservice/services/feign/InventoryServiceClient.java
@@ -1,4 +1,4 @@
-package mo.spring.ms.hpcbillingservice.services;
+package mo.spring.ms.hpcbillingservice.services.feign;
 
 import mo.spring.ms.hpcbillingservice.models.Product;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -6,11 +6,13 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-service")
+import javax.ws.rs.QueryParam;
+
+@FeignClient(name = "INVENTORY-SERVICE")
 public interface InventoryServiceClient {
     @GetMapping("/products/{id}?projection=fullProduct")
     Product findProductById(@PathVariable("id") Long id);
 
     @GetMapping("/products?projection=fullProduct")
-    PagedModel<Product> findAll();
+    PagedModel<Product> pageProducts(@QueryParam(value = "page") int page, @QueryParam(value = "size") int size);
 }
